e9c68e47738da79eb56b084013e3f8f2cf6443fc
Pikaboi <56373816+Pikaboi@users.noreply.github.com>
2021-09-03 10:19:44 +1200

    now with cash money

--- Assets/Scripts/TDTowerManager.cs
+++ Assets/Scripts/TDTowerManager.cs
@@ -16,12 +16,17 @@ public class TDTowerManager : MonoBehaviour
     public float m_cost;
     public float m_sellCost;
 
+    public float m_UG1Cost;
+    public float m_UG2Cost;
+
+    [SerializeField] PlayerResourceManager m_resource;
+
     // Start is called before the first frame update
     void Start()
     {
         m_child = Instantiate(m_base, transform.position, transform.rotation);
         m_child.transform.parent = gameObject.transform;
-
+        m_resource = FindObjectOfType<PlayerResourceManager>();
         m_sellCost = m_cost / 2;
     }
 
@@ -41,34 +46,63 @@ public class TDTowerManager : MonoBehaviour
 
     void Upgrade1()
     {
-        if(!m_UG2Bought)
+        if (!m_UG1Bought)
         {
-            Destroy(m_child);
-            m_child = Instantiate(m_upgrade1, transform.position, transform.rotation);
-            m_child.transform.parent = gameObject.transform;
-            m_UG1Bought = true;
-        } else
-        {
-            Destroy(m_child);
-            m_child = Instantiate(m_upgrade1n2, transform.position, transform.rotation);
-            m_child.transform.parent = gameObject.transform;
+            if (!m_UG2Bought)
+            {
+                if (m_resource.m_Money >= m_UG1Cost)
+                {
+                    Destroy(m_child);
+                    m_child = Instantiate(m_upgrade1, transform.position, transform.rotation);
+                    m_child.transform.parent = gameObject.transform;
+                    m_UG1Bought = true;
+                    m_resource.SubMoney(m_UG1Cost);
+                    m_sellCost += m_UG1Cost / 2;
+                }
+            }
+            else
+            {
+                if (m_resource.m_Money >= m_UG1Cost)
+                {
+                    Destroy(m_child);
+                    m_child = Instantiate(m_upgrade1n2, transform.position, transform.rotation);
+                    m_child.transform.parent = gameObject.transform;
+                    m_UG1Bought = true;
+                    m_resource.SubMoney(m_UG1Cost);
+                    m_sellCost += m_UG1Cost / 2;
+                }
+            }
         }
     }
 
     void Upgrade2()
     {
-        if (!m_UG1Bought)
+        if (!m_UG2Bought)
         {
-            Destroy(m_child);
-            m_child = Instantiate(m_upgrade2, transform.position, transform.rotation);
-            m_child.transform.parent = gameObject.transform;
-            m_UG2Bought = true;
-        }
-        else
-        {
-            Destroy(m_child);
-            m_child = Instantiate(m_upgrade1n2, transform.position, transform.rotation);
-            m_child.transform.parent = gameObject.transform;
+            if (!m_UG1Bought)
+            {
+                if (m_resource.m_Money >= m_UG2Cost)
+                {
+                    Destroy(m_child);
+                    m_child = Instantiate(m_upgrade2, transform.position, transform.rotation);
+                    m_child.transform.parent = gameObject.transform;
+                    m_UG2Bought = true;
+                    m_resource.SubMoney(m_UG2Cost);
+                    m_sellCost += m_UG2Cost / 2;
+                }
+            }
+            else
+            {
+                if (m_resource.m_Money >= m_UG2Cost)
+                {
+                    Destroy(m_child);
+                    m_child = Instantiate(m_upgrade1n2, transform.position, transform.rotation);
+                    m_child.transform.parent = gameObject.transform;
+                    m_UG2Bought = true;
+                    m_resource.SubMoney(m_UG2Cost);
+                    m_sellCost += m_UG2Cost / 2;
+                }
+            }
         }
     }
 }
