2541c6384866ad4d3d59bd4797edebf4f15b600f
Pikaboi <56373816+Pikaboi@users.noreply.github.com>
2021-09-02 12:23:07 +1200

    attack

--- Assets/Scripts/WorldCharacter.cs
+++ Assets/Scripts/WorldCharacter.cs
@@ -6,6 +6,16 @@ public class WorldCharacter : MonoBehaviour
 {
     CharacterController m_Controller;
     [SerializeField] float m_speed;
+
+    enum WeaponType
+    {
+        UNARMED,
+        MELEE,
+        RANGE
+    };
+
+    [SerializeField] WeaponType m_Equipped;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +24,11 @@ public class WorldCharacter : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
     {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
@@ -24,5 +39,23 @@ public class WorldCharacter : MonoBehaviour
         m_Controller.SimpleMove(dir * m_speed);
 
         transform.forward = dir;
+    }
+
+    void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            switch (m_Equipped)
+            {
+                case WeaponType.UNARMED:
+                    break;
+                case WeaponType.MELEE:
+                    break;
+                case WeaponType.RANGE:
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
