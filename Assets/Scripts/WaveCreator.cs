88d082f608a204683c6c275d8b5cb342eced524e
Pikaboi <56373816+Pikaboi@users.noreply.github.com>
2021-09-03 13:47:57 +1200

    yup

--- Assets/Scripts/WaveCreator.cs
+++ Assets/Scripts/WaveCreator.cs
@@ -10,16 +10,36 @@ public class WaveCreator : MonoBehaviour
     float[] enemyCount;
 
     List<TDEnemy> wave = new List<TDEnemy>();
+
+    public Transform m_destination;
+
     // Start is called before the first frame update
     void Start()
+    {
+        SetUpWave();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (wave.Count > 0)
+        {
+            TDEnemy e = Instantiate(wave[0], transform.position, transform.rotation);
+            e.m_Destination = m_destination;
+            wave.Remove(wave[0]);
+        }
+    }
+
+    void SetUpWave()
     {
         enemies = m_wave.GetTypes();
         enemyCount = m_wave.GetCount();
 
-        if(enemyCount.Length < enemies.Length)
+        if (enemyCount.Length < enemies.Length)
         {
             Debug.LogError("Wave Failed, the count array must be the same size");
-        } else
+        }
+        else
         {
             for (int i = 0; i < enemies.Length; i++)
             {
@@ -29,11 +49,5 @@ public class WaveCreator : MonoBehaviour
                 }
             }
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
