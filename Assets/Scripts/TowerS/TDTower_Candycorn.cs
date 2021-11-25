4c54e7e223f487d43037ef533e01c31c58ec326c
Pikaboi <56373816+Pikaboi@users.noreply.github.com>
2021-11-25 22:25:06 +1300

    finished!

--- Assets/Scripts/TowerS/TDTower_Candycorn.cs
+++ Assets/Scripts/TowerS/TDTower_Candycorn.cs
@@ -29,8 +29,10 @@ public class TDTower_Candycorn : TDTower
 
     public float effectiveRange = 0;
 
+    public bool bossPresent;
+
     // Start is called before the first frame update
-    void Start()
+    public override void Start()
     {
         base.Start();
         groupBonus = 0;
@@ -39,6 +41,21 @@ public class TDTower_Candycorn : TDTower
     // Update is called once per frame
     public override void Update()
     {
+        if (Path1UG3)
+        {
+            TDEnemy[] enems = GameObject.FindObjectsOfType<TDEnemy>();
+
+            bossPresent = false;
+
+            foreach(TDEnemy e in enems)
+            {
+                if (e.BossBool)
+                {
+                    bossPresent = true;
+                }
+            }
+        }
+
         if (Path3UG1 && m_Affinity == Affinity.MONSTER)
         {
             //I have no clue why i need to set the radius to 1 but uh yeah
@@ -82,7 +99,14 @@ public class TDTower_Candycorn : TDTower
             {
                 GameObject bullet = Instantiate(m_Projectile, transform.position + transform.forward * 1.5f, m_aimer.transform.rotation);
                 bullet.GetComponent<TDProjectile>().InheritFromTower(m_TriggerRange, m_attack + groupBonus + (m_attack * m_atkBuff), gameObject, m_Affinity);
-                m_FireTimer = m_fireRate;
+                if (bossPresent)
+                {
+                    m_FireTimer = m_fireRate / 2;
+                }
+                else
+                {
+                    m_FireTimer = m_fireRate;
+                }
                 if (GetComponentInParent<TDTowerManager>().m_ShootParticle != null)
                 {
                     GetComponentInParent<TDTowerManager>().m_ShootParticle.Play();
