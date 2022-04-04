e5b87868ecb467ce48047dce99ad1dcb7ca53935
Braden Cleur <braden.cleur@gmail.com>
2022-04-04 13:53:41 +1200

    barricade is made

--- Assets/Scripts/World/Barricade.cs
+++ Assets/Scripts/World/Barricade.cs
@@ -7,15 +7,18 @@ public class Barricade : MonoBehaviour
 {
     public float m_maxTimer;
     private float m_timer;
-    private Vector3 OGPos;
-    private bool m_moving;
+    private bool m_moving = false;
+    private bool m_moveBack = false;
     private PlayerResourceManager m_resource;
+    private Vector3 ogPos;
     public float m_price;
 
     void Start()
     {
         m_timer = m_maxTimer;
-        OGPos = transform.position;
+        m_resource = FindObjectOfType<PlayerResourceManager>();
+
+        ogPos = transform.position;
     }
 
     // Update is called once per frame
@@ -25,6 +28,11 @@ public class Barricade : MonoBehaviour
         {
             MoveToPosition();
         }
+
+        if(m_moveBack)
+        {
+            MoveBackToStart();
+        }
     }
 
     void MoveToPosition()
@@ -33,13 +41,27 @@ public class Barricade : MonoBehaviour
         posOnNavMesh.y = 0;
         NavMeshHit hit;
 
-        if (NavMesh.SamplePosition(posOnNavMesh, out hit, 1.0f, NavMesh.AllAreas))
+        if (!NavMesh.SamplePosition(posOnNavMesh, out hit, 1.0f, NavMesh.AllAreas))
+        {
+            transform.position += transform.forward * Time.deltaTime;
+        } else
         {
             BarricadeTimer();
         }
-        else
+    }
+
+    void MoveBackToStart()
+    {
+        Vector3 posOnNavMesh = transform.position;
+        posOnNavMesh.y = 0;
+        NavMeshHit hit;
+
+        if (Vector3.Distance(transform.position, ogPos) > 0.1f)
         {
-            transform.position += transform.forward * Time.deltaTime;
+            transform.position -= transform.forward * Time.deltaTime;
+        } else
+        {
+            m_moveBack = false;
         }
     }
 
@@ -47,10 +69,11 @@ public class Barricade : MonoBehaviour
     {
         m_timer -= Time.deltaTime;
 
-        if(m_timer > 0)
+        if(m_timer < 0)
         {
             m_timer = m_maxTimer;
             m_moving = false;
+            m_moveBack = true;
         }
     }
 
