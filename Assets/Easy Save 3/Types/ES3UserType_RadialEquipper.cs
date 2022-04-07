b80d09785f699f3e5f5115901fddb9b9d3a3dfed
Pikaboi <56373816+Pikaboi@users.noreply.github.com>
2022-04-07 12:12:50 +1200

    stuck on inventory resetting

---
+++ Assets/Easy Save 3/Types/ES3UserType_RadialEquipper.cs
@@ -0,0 +1,55 @@
+using System;
+using UnityEngine;
+
+namespace ES3Types
+{
+	[UnityEngine.Scripting.Preserve]
+	[ES3PropertiesAttribute("m_Weapon", "m_rot")]
+	public class ES3UserType_RadialEquipper : ES3ComponentType
+	{
+		public static ES3Type Instance = null;
+
+		public ES3UserType_RadialEquipper() : base(typeof(RadialEquipper)){ Instance = this; priority = 1;}
+
+
+		protected override void WriteComponent(object obj, ES3Writer writer)
+		{
+			var instance = (RadialEquipper)obj;
+			
+			writer.WritePropertyByRef("m_Weapon", instance.m_Weapon);
+			writer.WriteProperty("m_rot", instance.m_rot, ES3Type_Vector3.Instance);
+		}
+
+		protected override void ReadComponent<T>(ES3Reader reader, object obj)
+		{
+			var instance = (RadialEquipper)obj;
+			foreach(string propertyName in reader.Properties)
+			{
+				switch(propertyName)
+				{
+					
+					case "m_Weapon":
+						instance.m_Weapon = reader.Read<UnityEngine.GameObject>(ES3Type_GameObject.Instance);
+						break;
+					case "m_rot":
+						instance.m_rot = reader.Read<UnityEngine.Vector3>(ES3Type_Vector3.Instance);
+						break;
+					default:
+						reader.Skip();
+						break;
+				}
+			}
+		}
+	}
+
+
+	public class ES3UserType_RadialEquipperArray : ES3ArrayType
+	{
+		public static ES3Type Instance;
+
+		public ES3UserType_RadialEquipperArray() : base(typeof(RadialEquipper[]), ES3UserType_RadialEquipper.Instance)
+		{
+			Instance = this;
+		}
+	}
+}
