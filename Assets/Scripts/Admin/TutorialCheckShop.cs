946d849dd9b6b3f63b9c5ee3636845590a1123ad
Pikaboi <56373816+Pikaboi@users.noreply.github.com>
2022-05-25 10:14:55 +1200

    all tutorials are viewable

--- Assets/Scripts/Admin/TutorialCheckShop.cs
+++ Assets/Scripts/Admin/TutorialCheckShop.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class TutorialCheckShop : MonoBehaviour
 {
+    private bool callTutorial = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    public void playTutorial()
     {
-        
+        if (!callTutorial)
+        {
+            FindObjectOfType<TutorialControl>().ActivateTutorial(6);
+
+            TutorialCheckShop[] checks = FindObjectsOfType<TutorialCheckShop>(true);
+            
+            foreach (TutorialCheckShop check in checks)
+            {
+                check.callTutorial = true;
+            }
+        }
     }
 }
