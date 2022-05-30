e2d538e6e065bec89803c4c2e991bd266498d1ba
Braden Cleur <braden.cleur@gmail.com>
2022-05-30 16:11:11 +1200

    and now the icons encounter a problem

--- Assets/Scripts/UI/WeaponInventory/WheelEquipper.cs
+++ Assets/Scripts/UI/WeaponInventory/WheelEquipper.cs
@@ -9,9 +9,11 @@ public class WheelEquipper : MonoBehaviour
     public bool equipped = false;
     private TMPro.TMP_Text t;
     public UnityEngine.UI.Image m_weaponImage;
+    public Sprite blankImg;
     // Start is called before the first frame update
     void Start()
     {
+        blankImg = m_weaponImage.sprite;
         t = GetComponentInChildren<TMPro.TMP_Text>();
         if (equipped)
         {
@@ -22,7 +24,12 @@ public class WheelEquipper : MonoBehaviour
 
     private void Update()
     {
-        
+        if (m_weapon == null)
+        {
+            equipped = false;
+            t.text = "";
+            m_weaponImage.sprite = blankImg;
+        }
     }
 
     public void UpdateStats()
@@ -43,6 +50,9 @@ public class WheelEquipper : MonoBehaviour
     // Update is called once per frame
     public void OnClick()
     {
-        m_weapon.OnClick();
+        if (equipped)
+        {
+            m_weapon.OnClick();
+        }
     }
 }
