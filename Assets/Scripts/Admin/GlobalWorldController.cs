fd1bdb84f02a9e1b82a9531b1abe9269f2013cdd
Pikaboi <56373816+Pikaboi@users.noreply.github.com>
2022-06-15 17:00:14 +1200

    setting up scene one

--- Assets/Scripts/Admin/GlobalWorldController.cs
+++ Assets/Scripts/Admin/GlobalWorldController.cs
@@ -19,7 +19,7 @@ public class GlobalWorldController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        FindObjectOfType<TutorialControl>().ActivateTutorial(1);
+        //FindObjectOfType<TutorialControl>().ActivateTutorial(1);
     }
 
     // Update is called once per frame
